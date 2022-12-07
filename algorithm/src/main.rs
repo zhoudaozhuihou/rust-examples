cc2966a8b26af9679084a91dc6cc5083859e083e
pengfei <919275546@qq.com>
2022-12-07 21:36:13 +0800

    bulle sort

--- algorithm/src/main.rs
+++ algorithm/src/main.rs
@@ -1,18 +1,23 @@
 fn main() {
-    let mut ve1 = vec![5, -3, 0, 3, 1, 2, -1, 8, 7];
-    println!("排序前: {:?}",ve1);
-    bubble_sort(&mut ve1);
-    println!("排序后：{:?}",ve1);
+    // let mut v = vec![3, 2, 5, 1, 4];
+    let mut v = vec!['a', 'b', 'h', 'e', 'c'];
+    println!("before sort: v = {:?}", v);
+
+    bubble_sort(&mut v);
+    println!("after sort: v = {:?}", v);
 }
 
-fn bubble_sort<T: Ord>(arr: &mut [T]) {
-    for i in 0..arr.len() {
-        for j in 0..arr.len() - 1 - i {
-            if arr[j] > arr[j + 1] {
-                arr.swap(j, j + 1);
-            }
+
+#[allow(dead_code)]
+fn bubble_sort<T: PartialOrd>(v: &mut Vec<T>) {
+    let mut swapped = false;
+    for i in 0..(v.len() - 1) {
+        if v[i] > v[i + 1] {
+            v.swap(i, i + 1);
+            swapped = true;
         }
     }
+    if swapped {
+        bubble_sort(v);
+    }
 }
-
-
